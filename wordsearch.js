26441f7d448ea59bf514c95b2c39f911b19dd096
Jonathan Lao <jon.lao@gmail.com>
2023-11-22 19:52:14 -0600

    modified files in pair programming

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,30 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+
+    const wordSearch = (letters, word) => { 
+      const horizontalJoin = letters.map(ls => ls.join(''))
+      for (l of horizontalJoin) {
+          if (l.includes(word)) return true
+      }
+      // transpose matrix
+      let rowStr = []; 
+      for (let c =  0; c < letters[0].length; c++)
+      {
+          let vertWord = "";
+          for(let r = 0; r < letters.length; r++)
+          {
+              vertWord += letters[r][c];
+          }
+          rowStr.push(vertWord);
+      }
+      // look for the word
+      for(const value of rowStr)
+      {
+          if(value.includes(word))
+          {
+              return true;
+          }
+      }
+      return false;
+  }
+
 
 module.exports = wordSearch
